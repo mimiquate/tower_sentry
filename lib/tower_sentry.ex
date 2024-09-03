c3bbe8cf1132e756b5c3540d9a436614c79582f3
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-09-03 16:09:26 -0300

    mix new tower_sentry

---
+++ lib/tower_sentry.ex
@@ -0,0 +1,18 @@
+defmodule TowerSentry do
+  @moduledoc """
+  Documentation for `TowerSentry`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TowerSentry.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tower_sentry_test.exs
@@ -0,0 +1,8 @@
+defmodule TowerSentryTest do
+  use ExUnit.Case
+  doctest TowerSentry
+
+  test "greets the world" do
+    assert TowerSentry.hello() == :world
+  end
+end
