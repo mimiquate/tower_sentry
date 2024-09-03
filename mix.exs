d26b51a8d7f5935b27b7be6dbe3cc3fcb310fc36
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-09-03 16:14:14 -0300

    ci: enables

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TowerSentry.MixProject do
     [
       app: :tower_sentry,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
